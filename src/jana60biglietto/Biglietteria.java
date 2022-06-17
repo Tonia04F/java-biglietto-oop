fb3d1dce61a5099061d3a2f1c7176b2b93ee273a
tonia <tonia@cia.como>
2022-06-17 17:27:58 +0200

    fine compito

--- src/jana60biglietto/Biglietteria.java
+++ src/jana60biglietto/Biglietteria.java
@@ -1,10 +1,48 @@
 package jana60biglietto;
 
+import java.util.Scanner;
+
 public class Biglietteria {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		/*Aggiungere una classe Biglietteria, eseguibile con metodo main, in cui si chiede all’utente 
+		 * di inserire il numero di km e l’età del passeggero e 
+		 * stampa il prezzo del biglietto calcolato.*/
+
+		Scanner sc = new Scanner(System.in);
+		int km, etaPasseggero;
+		
+			System.out.println("Benvenuto, dove desidera andare?");
+			String destinazione = sc.nextLine();
+	do {
+		
+			System.out.println("quanti anni hai?: ");
+			etaPasseggero=Integer.parseInt(sc.nextLine());
+			
+			if(etaPasseggero<18) {
+				System.out.println("Sei minirenne per viaggiare devi essere accompagnato da un'adulto");
+			}
+			
+	}while(etaPasseggero<=18);
+	
+	
+	do {
+		
+		System.out.println("Quanti km devi percorrere: ");
+		km=sc.nextInt();
+		
+		if(km<20) {
+			System.out.println("Il viaggio breve supera i 20 km, non ci sono biglietti disponibili ");
+		}
+		
+}while(km<20);
+	
+	Biglietto ticket = new Biglietto(km, etaPasseggero);
+	System.out.println(ticket.calcolaPrezzo());
+		
+		
+		
+	
 
 	}
-
 }
--- src/jana60biglietto/Biglietto.java
+++ src/jana60biglietto/Biglietto.java
@@ -1,5 +1,75 @@
 package jana60biglietto;
 
 public class Biglietto {
+/*Creare una classe Biglietto con due attributi:
+ *  chilometri ed 
+ *  età del passeggero.
+ *  
+Utilizzare i modificatori dei livelli di accesso per proteggere il funzionamento interno della classe ed esporre solo un’interfaccia fatta di metodi public, come abbiamo visto in classe e nelle slide.
+La classe deve esporre un metodo che calcola il prezzo del biglietto secondo la logica:
+il prezzo del biglietto è definito in base ai km (0.21 € al km)
+va applicato uno sconto del 20% per i minorenni
+va applicato uno sconto del 40% per gli over 65*/
+	
+	//ATTRIBUTI
+	private int km;
+	private int etaPasseggero;
+	
+	private double prezzoBiglietto;
+	
+	
+	//COSTRUTTORE
+	public Biglietto(int km, int etaPasseggero) {
+		super();
+		this.km = km;
+		this.etaPasseggero = etaPasseggero;
+	}
 
+
+	//GETTER SETTER
+	
+	public int getKm() {
+		return km;
+	}
+
+
+	public void setKm(int km) {
+		this.km = km;
+	}
+
+
+	public int getEtaPasseggero() {
+		return etaPasseggero;
+	}
+
+
+	public void setEtaPasseggero(int etaPasseggero) {
+		this.etaPasseggero = etaPasseggero;
+	}
+	
+	/*il prezzo del biglietto è definito in base ai km (0.21 € al km)
+	 *va applicato uno sconto del 20% per i minorenni
+	 *va applicato uno sconto del 40% per gli over 65*/
+	
+	//creo metodo per calcolare prezzo del biglietto
+	public double calcolaPrezzo() {
+		
+		prezzoBiglietto = km * 0.21;
+		
+		if(etaPasseggero>65) {
+			prezzoBiglietto= (km*0.21) - (prezzoBiglietto*40%100);
+			System.out.println("Il passeggero è over 65 quindi viene applicato il 40% di sconto: " + prezzoBiglietto);
+		}
+		
+		else if(etaPasseggero <18) {
+			prezzoBiglietto = (km*0.21) - (prezzoBiglietto*20%100);
+			System.out.println("Il passeggero è minorenne quindi viene applicato il 20% di sconto: " + prezzoBiglietto);
+		}
+		else{
+			prezzoBiglietto = (km*0.21) ;
+			System.out.println("Il prezzo del biglietto è: " + prezzoBiglietto);
+		}
+		return prezzoBiglietto;
+		
+	}
 }
